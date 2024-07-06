76384a2cfdbed64b85e9540b2400c363dc884c6c
Luis-AP <lap18958@gmail.com>
2024-07-05 21:15:44 -0300

    Función reducer y hook useReducer

--- src/components/MusicPlayer/SongList.jsx
+++ src/components/MusicPlayer/SongList.jsx
@@ -1,25 +1,21 @@
 import useFetch from "../../hooks/useFetch";
 import SongCard from "./SongCard";
-import { useContext } from "react";
-import ThemeContext from "../../contexts/ThemeContext";
 
 function SongList() {
-    const { theme } = useContext(ThemeContext);
-
-    const [songs, isError, isLoading] = useFetch(
+    const { data, isError, isLoading } = useFetch(
         "https://sandbox.academiadevelopers.com/harmonyhub/songs/"
     );
 
     if (isLoading) return <p>Cargando...</p>;
     if (isError) return <p>Error al cargar las canciones.</p>;
-    if (!songs) return <p>No hay canciones disponibles</p>;
+    if (!data) return <p>No hay canciones disponibles</p>;
 
     return (
         <div>
             <div className="my-5">
                 <h2 className="title">Lista de Canciones</h2>
                 <ul>
-                    {songs.map((song) => (
+                    {data.map((song) => (
                         <div key={song.id} className="column is-two-third">
                             <SongCard song={song} />
                         </div>
